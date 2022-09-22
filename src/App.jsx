16d7663f603280c8e4c49de9fe5d43aa2fced132
Neilson Oliveira <106606239+neyprodigio@users.noreply.github.com>
2022-09-22 16:38:09 -0300

    Feat: LandPage concluido

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './reset.css';
+import './App.css';
+import imagem from './assets/illustration.svg';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <section className='info-home'>
+          <p className='title-p'>Nu <span className='title-span'>Kenzie</span></p>
+          <h1 className='text-center'>Centralize o controle das suas finanças</h1>
+          <p className='text-footer'>de forma rápida e segura</p>
+          <button className='btn-iniciar'>Iniciar</button>
+        </section>
+        <section className='img-section'>
+          <img className='img-rigth' src={imagem} alt="" />
+        </section>
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
